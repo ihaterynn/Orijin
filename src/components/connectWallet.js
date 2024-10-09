1d398c3f9451c5141d7b12b3e20a8350fe2a18b0
ihaterynn <ryanchanimpt77@gmail.com>
2024-10-09 08:01:56 +0800

    connect wallet button minor edit

--- src/components/connectWallet.js
+++ src/components/connectWallet.js
@@ -1,12 +1,16 @@
-// src/components/ConnectWallet.js
 import React from 'react';
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import { useWallet } from '@solana/wallet-adapter-react'; // Import the useWallet hook
 import './connectWallet.css';
 
 function ConnectWallet() {
+    const { connected } = useWallet(); // Destructure the 'connected' state
+
     return (
         <div>
-            <WalletMultiButton className="connect-wallet">CONNECT WALLET</WalletMultiButton> 
+            <WalletMultiButton className="connect-wallet">
+                {connected ? "CONNECTED" : "CONNECT WALLET"} {/* Conditional rendering */}
+            </WalletMultiButton>
         </div>
     );
 }
